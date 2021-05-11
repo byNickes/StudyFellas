8c687d26358de9d11781bac7e04a55c177411e78
Nicolò Baroncini <baroncini.1834907@studenti.uniroma1.it>
2021-05-11 20:01:27 +0200

    set google oauth login only with studenti.uniroma1.it and added bootstrap

--- StudyFellas/app/controllers/Users/omniauth_callbacks_controller.rb
+++ StudyFellas/app/controllers/Users/omniauth_callbacks_controller.rb
@@ -2,11 +2,17 @@
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
-    @user = User.from_omniauth(request.env["omniauth.auth"])
-    
-    flash[:notice] = I18n.t "devise.omniauth_callbacks.success",
-    kind: "Google"
-    
-    sign_in_and_redirect @user, event: :authentication
+    hd = request.env["omniauth.auth"]["extra"]["id_info"]["hd"]
+
+    if(hd == "studenti.uniroma1.it")
+      @user = User.from_omniauth(request.env["omniauth.auth"])
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success",
+      kind: "Google"
+      
+      sign_in_and_redirect @user, event: :authentication
+    else
+      redirect_to new_user_session_path
+    end
+
   end
 end
