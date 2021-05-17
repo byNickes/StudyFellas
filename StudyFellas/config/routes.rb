ad2d00202ba596fb35ce23a6e1861c2df4d242eb
Nicolò Baroncini <baroncini.1834907@studenti.uniroma1.it>
2021-05-17 19:55:55 +0200

    disabled unused devise routes

--- StudyFellas/config/routes.rb
+++ StudyFellas/config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, skip:[:passwords, :registrations], :controllers => { omniauth_callbacks: 'users/omniauth_callbacks' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # ROUTES PER LOGIN
+
+ # get '/users/sign_in', to: 'devise/sessions#new', :as => :new_user_session
+ # post '/users/sign_in', to: 'devise/sessions#create', :as => :user_session
+ # delete '/users/sign_out', to: 'devise/sessions#destroy', :as => :destroy_user_session
+
+  # devise_for:users PER TUTTE LE ROUTES
 end
