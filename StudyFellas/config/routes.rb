95cae09e3a533520c17fa263fc8a9bc43a755ea3
Nicolò Baroncini <baroncini.1834907@studenti.uniroma1.it>
2021-05-21 20:20:38 +0200

    created custom sessions_controller & added flash message for failed admin login

--- StudyFellas/config/routes.rb
+++ StudyFellas/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, skip:[:passwords, :registrations], :controllers => { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, skip:[:passwords, :registrations], :controllers => { omniauth_callbacks: 'users/omniauth_callbacks', sessions: 'users/sessions' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # ROUTES PER LOGIN
