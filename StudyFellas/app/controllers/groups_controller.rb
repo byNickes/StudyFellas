dd7cd13efd2355d94a17580ecb064076582c6e54
Leonardo <leonardocataldi2000@gmail.com>
2021-05-25 21:38:19 +0200

    group view updated, routes updated

--- StudyFellas/app/controllers/groups_controller.rb
+++ StudyFellas/app/controllers/groups_controller.rb
@@ -1,2 +1,20 @@
 class GroupsController < ApplicationController
+
+
+    def index
+        @groups = Group.all
+    end
+
+    def show
+        render html: "nel gruppo"
+    end
+
+    def new
+        render html: "nella fase di creazione"
+    end
+
+    def create
+    end
+
+
 end
--- StudyFellas/config/routes.rb
+++ StudyFellas/config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, skip:[:passwords, :registrations], :controllers => { omniauth_callbacks: 'users/omniauth_callbacks', sessions: 'users/sessions' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  #TEMPORARY ROOT
-    root :to => 'exams#index'
+  #ROOT
+    root :to => 'groups#index'
   ##
 
   get '/exams', to: 'exams#index', :as => :exams
@@ -14,4 +14,5 @@ Rails.application.routes.draw do
  # delete '/users/sign_out', to: 'devise/sessions#destroy', :as => :destroy_user_session
 
   # devise_for:users PER TUTTE LE ROUTES
+  resources :groups
 end
